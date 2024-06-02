a26c0de57ea3fe570880abc7b9c5f241f3e64225
SamSkull19 <sifatsamin19@gmail.com>
2024-06-02 22:37:36 +0600

    login page design done

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,23 +2,28 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./HomePage/Home";
 import App from "./App";
 import "./index.css";
+import Login from "./UserAuth/Login";
 
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <App></App>,
-    //   errorElement: <ErrorPage></ErrorPage>,
-      children: [
-        {
-          path: "/",
-          element: <Home></Home>
+        path: "/",
+        element: <App></App>,
+        //   errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
+            },
+            
 
-        },
-    
-    ]
+        ]
     },
-  
-  ]);
-  
-  export default router;
+
+]);
+
+export default router;
