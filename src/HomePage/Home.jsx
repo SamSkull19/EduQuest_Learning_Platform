6731d9256d39b4a1211a9b06fa761b04dd1932c3
SamSkull19 <sifatsamin19@gmail.com>
2024-06-03 13:40:13 +0600

    our partner sectin added

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -1,16 +1,23 @@
 import Banner from "./Banner";
+import OurPartner from "./OurPartner";
 
 const Home = () => {
     return (
-        <div className='bg-teal-800'>
-            <div className="max-w-[1170px] mx-auto pt-5">
-                <div className='bg-cover bg-center h-[500px] rounded-2xl'>
-                    <div className="absolute h-[500px] w-[1170px] bg-black opacity-50 rounded-2xl"></div>
-                    <Banner></Banner>
+        <>
+            <div className='bg-teal-800'>
+                <div className="max-w-[1170px] mx-auto pt-5">
+                    <div className='bg-cover bg-center h-[500px] rounded-2xl'>
+                        <div className="absolute h-[500px] w-[1170px] bg-black opacity-50 rounded-2xl"></div>
+                        <Banner></Banner>
+                    </div>
+                    <div className='h-[50px]'></div>
                 </div>
-                <div className='h-[50px]'></div>
             </div>
-        </div>
+
+            <div className="max-w-[1170px] mx-auto pt-5">
+                <OurPartner></OurPartner>
+            </div>
+        </>
     );
 };
 
---
+++ src/HomePage/OurPartner.jsx
@@ -0,0 +1,57 @@
+import { SiSkillshare } from "react-icons/si";
+import { SiDatacamp } from "react-icons/si";
+import { SiUdemy } from "react-icons/si";
+import { SiCoursera } from "react-icons/si";
+import { SiCisco } from "react-icons/si";
+import { FaGithubSquare } from "react-icons/fa";
+import { FaFreeCodeCamp } from "react-icons/fa";
+
+
+
+const OurPartner = () => {
+    return (
+        <div className="my-20 roboto">
+            <h1 className="text-4xl font-bold text-center mb-5">Our Partners and Collaborators</h1>
+            <p className="text-center mb-10">At EduQuest, our partners and collaborators are integral to our mission. By joining forces with renowned educational institutions and industry leaders, we bring cutting-edge content and real-world expertise to empower learners worldwide.</p>
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 text-9xl gap-8 justify-between">
+                <div className="flex flex-col items-center">
+                    <SiUdemy />
+                    <p className="text-2xl mt-2 font-semibold">Udemy</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <SiCoursera />
+                    <p className="text-2xl mt-2 font-semibold">Coursera</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <SiDatacamp />
+                    <p className="text-2xl mt-2 font-semibold">DataCamp</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <SiSkillshare />
+                    <p className="text-2xl mt-2 font-semibold">SkillShare</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <SiCisco />
+                    <p className="text-2xl mt-2 font-semibold">Cisco</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <FaGithubSquare />
+                    <p className="text-2xl mt-2 font-semibold">Github</p>
+                </div>
+
+                <div className="flex flex-col items-center">
+                    <FaFreeCodeCamp />
+                    <p className="text-2xl mt-2 font-semibold">FreeCodeCamp</p>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default OurPartner;
