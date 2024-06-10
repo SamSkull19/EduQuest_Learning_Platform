fd1fa9392656ce20af0ad2dbe665955dc56157c6
SamSkull <sifatsamin19@gmail.com>
2024-06-10 16:20:34 +0600

    assignment submit and per day assignment count done

--- src/StudentDashboard/MyClassDetails.jsx
+++ src/StudentDashboard/MyClassDetails.jsx
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { axiosSecure } from '../Hooks/useAxiosSecure';
 
 const MyClassDetails = () => {
     const { id } = useParams();
@@ -13,21 +15,53 @@ const MyClassDetails = () => {
                 ),
     })
 
-    if (myCoursePending) {
+    const { isPending: myAssignmentPending, data: assignments } = useQuery({
+        queryKey: ['assignments'],
+        queryFn: () =>
+            fetch('http://localhost:5000/assignments')
+                .then((res) =>
+                    res.json(),
+                ),
+    })
+
+    if (myCoursePending || myAssignmentPending) {
         return <div className="flex justify-center items-center"><span className="loading loading-spinner loading-lg"></span></div>
     }
 
+
+
     const courseDetail = course && course.find(course => course._id === id);
 
-    console.log(courseDetail);
+    const { name, image, shortdescription } = courseDetail;
 
-    if (!courseDetail) {
-        return <div>Details not found.</div>;
-    }
+    const courseAssignments = assignments.find(assignment => assignment.courseId === id);
 
-    const { _id, title, name, image, shortdescription, totalenrolment } = courseDetail;
+    console.log(courseAssignments);
 
-    console.log(course);
+
+    const { _id, title, deadline, assignmentDescription } = courseAssignments;
+
+    const handleSubmit = async (assignmentId) => {
+        console.log(assignmentId);
+        const response = await axiosSecure.put(`/assignments/submit/${assignmentId}`);
+        if (response.status === 200) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Assignment Submitted Successfully!',
+                showConfirmButton: false,
+                timer: 1500
+            });
+        }
+        else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Something went wrong!',
+            });
+        }
+
+    };
+
 
     return (
         <div>
@@ -55,10 +89,12 @@ const MyClassDetails = () => {
                     <tbody>
                         <tr key={_id} className='text-white'>
                             <td className="py-2 px-4 border border-red-900">{title}</td>
-                            <td className="py-2 px-4 border border-red-900">{shortdescription}</td>
-                            <td className="py-2 px-4 border border-red-900">{totalenrolment}</td>
+                            <td className="py-2 px-4 border border-red-900">{assignmentDescription}</td>
+                            <td className="py-2 px-4 border border-red-900">{deadline}</td>
                             <td className="py-2 px-4 border border-red-900">
-                                <button className="bg-orange-500 hover:bg-teal-200 py-1 px-3 rounded">Submit</button>
+                                <button onClick={() => handleSubmit(_id)} className="bg-orange-500 hover:bg-teal-200 py-1 px-3 rounded">
+                                    Submit
+                                </button>
                             </td>
                         </tr>
 
