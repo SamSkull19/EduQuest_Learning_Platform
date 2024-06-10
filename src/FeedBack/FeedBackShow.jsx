76be79a507062b9adef2935edb4b85d73242654f
SamSkull <sifatsamin19@gmail.com>
2024-06-10 18:48:15 +0600

    feedback show fixed

--- src/FeedBack/FeedBackShow.jsx
+++ src/FeedBack/FeedBackShow.jsx
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 
 const FeedBackShow = ({ feedback }) => {
     console.log(feedback);
-    const { title, name, image } = feedback;
+    const { title, name, image, rating } = feedback;
 
     return (
         <div className='bg-orange-200 rounded-3xl w-[370px] h-[580px] -z-10 border border-blue-950 p-5'>
-            <div className=''>
+            <div>
                 <img src={image} className='w-[340px] h-[340px] rounded-3xl mx-auto' alt="slide_image" />
             </div>
-            <div className='p-7'>
-                <h1 className='text-3xl font-bold text-teal-800'>{name}</h1>
+            <div className='px-7 pt-2'>
+                <h3 className='text-sm mt-2 text-stone-700 font-semibold py-2'>Rating : {rating}</h3>
+
+                <h1 className='text-3xl font-bold text-teal-800 '>{name}</h1>
 
                 <h3 className='text-xl mt-2 text-stone-700 font-semibold'>{title}</h3>
 
